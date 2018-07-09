e9399b7d529180b79f1c78260d0c8b2852e211d2
Tom Almeida <tommoa256@gmail.com>
2018-07-09 23:12:14 +0800

    Added crate overview to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//!
+//! # Small
+//!
+//! Small is a crate devoted to "small" types - those that store additional
+//! information on the stack while they can before expanding to the heap.
+//! This leads to better performance for lower amounts of storage as you do not
+//! have to wait for allocation between insertions and deletions.
+//!
+//! Currently, only the [`String`] type has been implemented. This is based
+//! upon similar implementations of `String` in various libc++ libraries.
+//! `String` stores 23 bytes of data on the stack, however once it begins to
+//! use the heap to store data, it never returns to using the stack.
+//!
+//! [`String`]: string/struct.String.html
+
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(alloc))]
 
